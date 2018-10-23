86c6f2998aef6caffae5cee8da2afd3827721dc7
Colm Doherty <G00364795@gmit.ie>
2018-10-23 15:43:35 +0100

    not moved from Repos

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@
 print("Hello World")
 # git & cloned from github
 print("now pushed from CMD lin")
-# test before moving to REPOS directory
-print("test from Anscombes folder")
+# test after not moving to REPOS directory
+print("test not from REPOS folder")
 
