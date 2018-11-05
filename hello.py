62a320def161cdcc139c9130b7184a95eb106f50
Colm Doherty <G00364795@gmit.ie>
2018-11-05 21:51:20 +0000

    uploaded csv file

--- hello.py
+++
@@ -1,7 +0,0 @@
-# Colm Doherty 22 Oct 2018
-print("Hello World")
-# git & cloned from github
-print("now pushed from CMD lin")
-# test after not moving to REPOS directory
-print("test not from REPOS folder")
-
